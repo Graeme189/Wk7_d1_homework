0cd4e5d2d915767e3c417f2650e8274aee15b423
Graeme Henderson <graeme.189@gmail.com>
2019-08-05 15:28:50 +0100

    Basic build complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      tasks: [
+        {name: 'Wash dishes', completed: false},
+        {name: 'Clean shower', completed: false},
+        {name: 'Hoover living room', completed: false}
+      ],
+      newTask: ''
+    },
+    methods: {
+      saveNewTask: function (e) {
+        e.preventDefault();
+        this.tasks.push({
+          name: this.newTask,
+          completed: false
+        });
+        this.newTask = '';
+      },
+      completeTask: function (index) {
+        this.tasks[index].completed = true;
+      }
+    }
+  });
+});
